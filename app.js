ced83e41f7d40d92e122b82f74a7356b391a3506
Kota 'dublook' FUTAMI <kota.futami@gmail.com>
2018-01-05 15:48:35 +0900

    refs #1 Echo URL parameter

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.get('/regza/volume/up/:delta', function(req, res) {
+  var delta = req.params.delta;
+  res.send({
+    "requestedAction": "regza-volume-up",
+    "param-delta": delta
+  });
+});
+
 app.get('/', function(req, res) {
   res.send({
     "Output": "Hello World!"
