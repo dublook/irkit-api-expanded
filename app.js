cb8ef375fdf5117f08acac7acbd3ed8e1fa7c69f
Kota 'dublook' FUTAMI <kota.futami@gmail.com>
2018-01-05 16:25:10 +0900

    refs #1 Fix HTTP method

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/regza/volume/up/:delta', function(req, res) {
+app.post('/regza/volume/up/:delta', function(req, res) {
   var delta = req.params.delta;
   res.send({
     "requestedAction": "regza-volume-up",
